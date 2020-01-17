f374ad34e5ed06237dadcff344c71b1d252d2929
rkarl2 <59965479+rkarl2@users.noreply.github.com>
2020-01-16 21:18:16 -0600

    Update
    Hey Thomas, I added my part, I also added the top part for you!

--- hello.c
+++ hello.c
@@ -1,7 +1,20 @@
+/*
+Author:Thomas Hervert
+1/16/2020
+Collaborators: Ryan Karl
+Purpose: Print name and major
+*/
+
 #include <stdio.h>
 
 int main(void) {
 	printf("Thomas Hervert\n");
+	
 	printf("Electrical Engineering\n");
+	
+	printf("Ryan Karl\n");
+	
+	printf("Electrical Engineering\n");
+	
 	return 0;
 }
